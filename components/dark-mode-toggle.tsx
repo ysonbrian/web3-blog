61b54b3b691418fab32f0a304b766d35725d55af
ysonbrian <ysonbrian@gmail.com>
2024-02-07 16:52:45 +0900

    Init settings

---
+++ components/dark-mode-toggle.tsx
@@ -0,0 +1,32 @@
+"use client"
+
+import * as React from "react"
+import { MoonIcon, SunIcon } from "@radix-ui/react-icons"
+import { useTheme } from "next-themes"
+
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+export default function DarkModeToggle() {
+  const { resolvedTheme, setTheme } = useTheme()
+  const toggleTheme = () => {
+    if (resolvedTheme === "dark") {
+      setTheme("light")
+    } else {
+      setTheme("dark")
+    }
+  }
+  return (
+    <>
+      <Button variant="outline" size="icon" onClick={toggleTheme}>
+        <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+        <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+      </Button>
+    </>
+  )
+}
---
+++ components/navbar.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import DarkModeToggle from "./dark-mode-toggle"
+
+export default function Navbar() {
+  return (
+    <div>
+      <DarkModeToggle />
+    </div>
+  )
+}
